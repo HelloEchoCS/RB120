e54f6e1e710026645a3211c9610f0d31ad571ad8
Chris Shen <olsonshen@gmail.com>
2022-02-18 02:57:56 -0500

    Added Base TTT Game

--- lesson_2/test.rb
+++ lesson_2/test.rb
@@ -1,13 +1,71 @@
-class Test
-  def self.a_method
-    42
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
   end
 end
 
-class Child < Test
-  
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
 end
 
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
 
-p Test.a_method
-p Child.a_method
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Boat
+  include Moveable
+  attr_accessor :propeller_count, :hull_count
+
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    super + 10
+  end
+end
+
+class Catamaran < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.propeller_count = 2
+    self.hull_count = 2
+    # ... other code to track catamaran-specific data omitted ...
+  end
+end
+
+class Motorboat < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.propeller_count = 1
+    self.hull_count = 1
+  end
+end
