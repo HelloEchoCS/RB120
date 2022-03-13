3c14aaa71319945650bdc234863e5d059fd74d51
Chris Shen <olsonshen@gmail.com>
2022-03-13 18:43:51 -0400

    Final Update

---
+++ lesson_5/accessment.rb
@@ -0,0 +1,40 @@
+class AnimalClass
+  attr_accessor :name, :animals
+
+  def initialize(name)
+    @name = name
+    @animals = []
+  end
+
+  def <<(animal)
+    animals << animal
+  end
+
+  def +(other_class)
+    new_obj = AnimalClass.new('NoName')
+    new_obj.animals = animals + other_class.animals
+    new_obj
+  end
+end
+
+class Animal
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+mammals = AnimalClass.new('Mammals')
+mammals << Animal.new('Human')
+mammals << Animal.new('Dog')
+mammals << Animal.new('Cat')
+
+birds = AnimalClass.new('Birds')
+birds << Animal.new('Eagle')
+birds << Animal.new('Blue Jay')
+birds << Animal.new('Penguin')
+
+some_animal_classes = mammals + birds
+
+p some_animal_classes
